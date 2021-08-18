d21b671445fca65d2c69bf7c236ec33231b129c4
S-noops <66436663+S-noops@users.noreply.github.com>
2021-08-18 14:53:34 -0500

    fix nav-bar hyperlinks
    This fixed the functionality of nav-bar hyperlinks while using flask

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask  import Flask, render_template
+from flask  import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/about")
