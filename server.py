ad0057891c097b56d17cfb5d443710e53a376adf
S-noops <66436663+S-noops@users.noreply.github.com>
2021-08-20 16:14:36 -0500

    add basic html for password generator page
    This simply adds the password generator page and an the options available for generating a password

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-from flask  import Flask, render_template, url_for
+from flask  import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+# route to homepage
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# route to about page
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+# route to password generator page
+@app.route("/passwords")
+def pass_gen():
+    return render_template("pass_generator.html")
+
+# Password generator Form Submission Rout
 
 if __name__ == "__main__":
     app.run(debug=True)
